3b270312b7bdeea882e6f98db3fb0c9efc81bf38
soumyag171 <100073291+soumyag171@users.noreply.github.com>
2026-09-23 08:17:41 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else if(st.isEmpty()) return false;
+            else if(c==')' && st.peek()=='(' || c=='}' && st.peek()=='{' || c==']' && st.peek()=='[')
+            st.pop();
+            else return false;
+        }
+        return st.isEmpty();
+    }
+}
